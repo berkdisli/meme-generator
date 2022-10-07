53eb1802c0e9a65be240fbe2b31228b06ab1a9c1
Berk Disli <berkdisli@yahoo.com>
2022-10-07 11:36:37 +0200

    handleChange, onchange, css

--- src/Components/MemeGenerator/memeGenerator.js
+++ src/Components/MemeGenerator/memeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
             randomImg: 'http://i.imgflip.com/1bij.jpg',
             allMemeImgs: []
         };
+        this.handleChange = this.handleChange.bind(this)
     }
 
     componentDidMount() {
@@ -21,25 +22,41 @@ class MemeGenerator extends Component {
                 this.setState({ allMemeImgs: memes })
             })
     }
+
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
     render() {
         return (
-            <div>
+            <div className='gen'>
                 <form className="meme-form">
                     <input
                         type='text'
                         name='topText'
                         placeholder='Top Text'
                         value={this.state.topText}
+                        onChange={this.handleChange}
+                        onfocus="this.value=''; this.color='blue'"
+
                     />
                     <input
                         type='text'
                         name='bottomText'
                         placeholder='Bottom Text'
                         value={this.state.bottomText}
+                        onChange={this.handleChange}
                     />
                     <button>Gen</button>
                 </form>
+                <div className='meme'>
+                    <img src={this.state.randomImg} alt='' />
+                    <h2 className='top'>{this.state.topText}</h2>
+                    <h2 className='bottom'>{this.state.bottomText}</h2>
+                </div>
             </div>
+
         )
     }
 }
